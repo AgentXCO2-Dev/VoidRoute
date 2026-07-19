0d3bfc232fe092f559627c62c705440994d7bc13
AgentXCO2 <thelittlestagent@gmail.com>
2026-07-19 16:09:18 -0500

    Add Electron main process foundation

---
+++ src/main/electron.ts
@@ -0,0 +1,22 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+function createWindow() {
+  const window = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  window.loadFile(path.join(__dirname, '../renderer/index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
